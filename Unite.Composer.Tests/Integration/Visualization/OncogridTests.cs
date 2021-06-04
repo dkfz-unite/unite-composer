6c5421dcd5332b0b114d59feb794b425d20eeb82
Valiantsin Ulasau <valiantsin.ulasau@gmail.com>
2021-06-04 14:25:47 +0200

    Unite tests.

---
+++ Unite.Composer.Tests/Integration/Visualization/OncogridTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unite.Composer.Tests.Integration.Visualization
+{
+    [TestClass]
+    public class OncogridTests
+    {
+        public OncogridTests()
+        {
+        }
+
+        [TestMethod]
+        public void CheckPerformanse()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
