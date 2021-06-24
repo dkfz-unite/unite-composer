1f7a6a03fd7e0c9ab9534b28f81ebc6418f23368
Valiantsin Ulasau <valiantsin.ulasau@gmail.com>
2021-06-24 22:17:05 +0200

    Organoids.

--- Unite.Composer/Search/Services/Criteria/XenograftCriteria.cs
+++ Unite.Composer/Search/Services/Criteria/XenograftCriteria.cs
@@ -5,10 +5,9 @@ namespace Unite.Composer.Search.Services.Criteria
     public class XenograftCriteria : SpecimenCriteria
     {
         public string[] MouseStrain { get; set; }
+        public string[] Intervention { get; set; }
+        public Range<double?> SurvivalDays { get; set; }
         public bool? Tumorigenicity { get; set; }
         public string[] TumorGrowthForm { get; set; }
-        public Range<double?> SurvivalDays { get; set; }
-
-        public string[] Intervention { get; set; }
     }
 }
