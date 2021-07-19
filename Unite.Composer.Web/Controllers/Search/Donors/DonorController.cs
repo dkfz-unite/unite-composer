cfc12841304755bf22975553b0745e58f0372a76
Valiantsin Ulasau <valiantsin.ulasau@gmail.com>
2021-07-19 12:14:49 +0200

    Switching DonorController to ApiController mode.

--- Unite.Composer.Web/Controllers/Search/Donors/DonorController.cs
+++ Unite.Composer.Web/Controllers/Search/Donors/DonorController.cs
@@ -1,32 +1,69 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Unite.Composer.Resources.Mutations;
+using Unite.Composer.Search.Engine.Queries;
 using Unite.Composer.Search.Services;
+using Unite.Composer.Search.Services.Context;
+using Unite.Composer.Search.Services.Criteria;
 using Unite.Composer.Web.Configuration.Filters.Attributes;
 using Unite.Composer.Web.Resources.Donors;
-using Unite.Indices.Entities.Donors;
+using Unite.Composer.Web.Resources.Specimens;
+
+using DonorIndex = Unite.Indices.Entities.Donors.DonorIndex;
+using MutationIndex = Unite.Indices.Entities.Mutations.MutationIndex;
+using SpecimenIndex = Unite.Indices.Entities.Specimens.SpecimenIndex;
 
 namespace Unite.Composer.Web.Controllers.Search.Donors
 {
     [Route("api/[controller]")]
+    [ApiController]
     public class DonorController : Controller
     {
-        private readonly ISearchService<DonorIndex> _searchService;
+        private readonly ISearchService<DonorIndex> _donorsSearchService;
+        private readonly ISearchService<MutationIndex> _mutationsSearchService;
+        private readonly ISearchService<SpecimenIndex, SpecimenSearchContext> _spesimensSearchService;
 
 
-        public DonorController(ISearchService<DonorIndex> searchService)
+        public DonorController(
+            ISearchService<DonorIndex> donorsSearchService,
+            ISearchService<MutationIndex> mutationsSearchService,
+            ISearchService<SpecimenIndex, SpecimenSearchContext> specimensSearchService)
         {
-            _searchService = searchService;
+            _donorsSearchService = donorsSearchService;
+            _mutationsSearchService = mutationsSearchService;
+            _spesimensSearchService = specimensSearchService;
         }
 
 
-        [HttpGet]
+        [HttpGet("{id}")]
         [CookieAuthorize]
         public DonorResource Get(int id)
         {
             var key = id.ToString();
 
-            var index = _searchService.Get(key);
+            var index = _donorsSearchService.Get(key);
 
             return From(index);
+        }
+
+        [HttpPost("{id}/mutations")]
+        [CookieAuthorize]
+        public SearchResult<MutationResource> GetMutations(int id, [FromBody] SearchCriteria searchCriteria)
+        {
+            var searchResult = _mutationsSearchService.Search(searchCriteria);
+
+            return From(searchResult);
+        }
+
+        [HttpPost("{id}/specimens")]
+        [CookieAuthorize]
+        public SearchResult<SpecimenResource> GetSpecimens(int id, [FromBody] SearchCriteria searchCriteria)
+        {
+            var searchContext = new SpecimenSearchContext();
+
+            var searchResult = _spesimensSearchService.Search(searchCriteria, searchContext);
+
+            return From(searchResult);
         }
 
 
@@ -38,6 +75,24 @@ namespace Unite.Composer.Web.Controllers.Search.Donors
             }
 
             return new DonorResource(index);
+        }
+
+        private SearchResult<MutationResource> From(SearchResult<MutationIndex> searchResult)
+        {
+            return new SearchResult<MutationResource>()
+            {
+                Total = searchResult.Total,
+                Rows = searchResult.Rows.Select(index => new MutationResource(index)).ToArray()
+            };
+        }
+
+        private SearchResult<SpecimenResource> From(SearchResult<SpecimenIndex> searchResult)
+        {
+            return new SearchResult<SpecimenResource>()
+            {
+                Total = searchResult.Total,
+                Rows = searchResult.Rows.Select(index => new SpecimenResource(index)).ToArray()
+            };
         }
     }
 }
