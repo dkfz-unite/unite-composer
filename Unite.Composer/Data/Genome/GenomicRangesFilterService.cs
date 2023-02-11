ee61fe935048a4a91c0f95816909ce1f82f4968e
vulasau <valiantsin.ulasau@gmail.com>
2023-02-11 14:24:22 +0100

    Even chromosome ranges calculation.

--- Unite.Composer/Data/Genome/GenomicRangesFilterService.cs
+++ Unite.Composer/Data/Genome/GenomicRangesFilterService.cs
@@ -21,14 +21,10 @@ public class GenomicRangesFilterService
 
         chromosomeRanges.Last().End = criteria.End;
 
-        long totalLength = chromosomeRanges.Sum(range => (long)range.End - (long)range.Start + 1);
+        var slice = (int)Math.Round((double)(filterCriteria.Length / filterCriteria.Density) + SHIFT, 0);
 
         foreach (var chromosomeRange in chromosomeRanges)
         {
-            var percent = Math.Round((double)(chromosomeRange.Length * 100.0000 / totalLength), 4);
-            var parts = (int)Math.Round((double)(criteria.Density / 100.0000 * percent), 0);
-            var slice = (int)Math.Round((double)(chromosomeRange.Length / parts + SHIFT), 0);
-
             var coveredLength = chromosomeRange.Start;
 
             do
@@ -37,12 +33,46 @@ public class GenomicRangesFilterService
                 var start = chromosomeRange.Start == coveredLength ? chromosomeRange.Start : coveredLength + 1;
                 var end = start + slice < chromosomeRange.End ? start + slice : chromosomeRange.End;
                 coveredLength = end;
-                //coveredLength += slice;
-                //var end = coveredLength < chromosomeRange.End ? coveredLength : chromosomeRange.End;
 
                 yield return new GenomicRange(chr, start, end);
             }
             while (coveredLength < chromosomeRange.End);
         }
     }
+
+    //public IEnumerable<GenomicRange> GetRanges(GenomicRangesFilterCriteria filterCriteria)
+    //{
+    //    var criteria = filterCriteria ?? new GenomicRangesFilterCriteria();
+
+    //    var chromosomeRanges = ChromosomeRanges.All
+    //        .Where(range => range.Chr >= criteria.StartChr && range.Chr <= criteria.EndChr)
+    //        .Select(range => new GenomicRange(range.Chr, range.Start, range.End))
+    //        .ToArray();
+
+    //    chromosomeRanges.First().Start = criteria.Start;
+
+    //    chromosomeRanges.Last().End = criteria.End;
+
+    //    long totalLength = chromosomeRanges.Sum(range => (long)range.End - (long)range.Start + 1);
+
+    //    foreach (var chromosomeRange in chromosomeRanges)
+    //    {
+    //        var percent = Math.Round((double)(chromosomeRange.Length * 100.0000 / totalLength), 4);
+    //        var parts = (int)Math.Round((double)(criteria.Density / 100.0000 * percent), 0);
+    //        var slice = (int)Math.Round((double)(chromosomeRange.Length / parts + SHIFT), 0);
+
+    //        var coveredLength = chromosomeRange.Start;
+
+    //        do
+    //        {
+    //            var chr = chromosomeRange.Chr;
+    //            var start = chromosomeRange.Start == coveredLength ? chromosomeRange.Start : coveredLength + 1;
+    //            var end = start + slice < chromosomeRange.End ? start + slice : chromosomeRange.End;
+    //            coveredLength = end;
+
+    //            yield return new GenomicRange(chr, start, end);
+    //        }
+    //        while (coveredLength < chromosomeRange.End);
+    //    }
+    //}
 }
--- Unite.Composer/Data/Genome/Models/GenomicRangesFilterCriteria.cs
+++ Unite.Composer/Data/Genome/Models/GenomicRangesFilterCriteria.cs
@@ -9,13 +9,16 @@ public class GenomicRangesFilterCriteria
     private int _endChr;
     private int _end;
     private int _density;
+    private bool _even = true;
     private bool _male = true;
 
     public int StartChr { get => GetStartChr(); set => _startChr = value; }
     public int Start { get => GetStart(); set => _start = value; }
     public int EndChr { get => GetEndChr(); set => _endChr = value; }
     public int End { get => GetEnd(); set => _end = value; }
+    public long Length { get => GetLength(); }
     public int Density { get => GetDensity(); set => _density = value; }
+    public bool Even { get => _even; set => _even = value; }
     public bool Male { get => _male; set => _male = value; }
 
 
@@ -58,22 +61,38 @@ public class GenomicRangesFilterCriteria
              : range.End;
     }
 
-    private int GetDensity()
+    private long GetLength()
     {
         if (StartChr == EndChr)
         {
-            var length = End - Start;
-
-            return _density > length ? length
-                 : _density > 2048 ? 2048
-                 : _density > 1024 ? 1024
-                 : 512;
+            return End - Start + 1;
         }
         else
         {
-            return _density > 2048 ? 2048
-                 : _density > 1024 ? 1024
-                 : 512;
+            long length = 0;
+
+            for (int i = StartChr; i <= EndChr; i++)
+            {
+                var range = ChromosomeRanges.All[i - 1];
+
+                length += i == StartChr ? range.End - Start + 1
+                        : i == EndChr ? End
+                        : range.End;
+            }
+
+            return length;
         }
     }
+
+    private int GetDensity()
+    {
+        var length = GetLength();
+
+        return _density >= length ? (int)length
+             : _density >= 2048 ? 2048
+             : _density >= 1024 ? 1024
+             : 512;
+    }
+
+    
 }
