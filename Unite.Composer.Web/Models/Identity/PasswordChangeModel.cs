f61f57a66b225c2fb4c74ae34f84119366972397
DGitRich <daniel.rich@gmx.net>
2023-05-23 16:59:33 +0200

    Delete PasswordChangeModel
    Remove identity related stuff

--- Unite.Composer.Web/Models/Identity/PasswordChangeModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Unite.Composer.Web.Models.Identity;
-
-public class PasswordChangeModel
-{
-    public string OldPassword { get; set; }
-    public string NewPassword { get; set; }
-    public string NewPasswordRepeat { get; set; }
-}
