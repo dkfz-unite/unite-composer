d67de3632ec4215cd8486cee91913ef1a9726c58
vulasau <valiantsin.ulasau@gmail.com>
2024-08-01 18:56:46 +0200

    Oncogrid updates.

--- Unite.Composer/Visualization/Oncogrid/Data/OncoGridDonor.cs
+++ Unite.Composer/Visualization/Oncogrid/Data/OncoGridDonor.cs
@@ -7,9 +7,17 @@ public class OncoGridDonor
     public string Id { get; }
     public string DisplayId { get; }
 
+    public string Diagnosis { get; }
+    public string PrimarySite { get; }
+    public string Localization { get; }
     public string Gender { get; }
     public int? Age { get; }
     public bool? VitalStatus { get; }
+    public int? VitalStatusChangeDay { get; }
+    public bool? ProgressionStatus { get; }
+    public int? ProgressionStatusChangeDay { get; }
+    public int? KpsBaseline { get; }
+    public bool? SteroidsBaseline { get; }
 
 
     public OncoGridDonor(DonorIndex index)
@@ -17,8 +25,16 @@ public class OncoGridDonor
         Id = index.Id.ToString();
         DisplayId = index.ReferenceId;
 
+        Diagnosis = index.ClinicalData?.Diagnosis;
+        PrimarySite = index.ClinicalData?.PrimarySite;
+        Localization = index.ClinicalData?.Localization;
         Gender = index.ClinicalData?.Gender;
         Age = index.ClinicalData?.Age;
         VitalStatus = index.ClinicalData?.VitalStatus;
+        VitalStatusChangeDay = index.ClinicalData?.VitalStatusChangeDay;
+        ProgressionStatus = index.ClinicalData?.ProgressionStatus;
+        ProgressionStatusChangeDay = index.ClinicalData?.ProgressionStatusChangeDay;
+        KpsBaseline = index.ClinicalData?.KpsBaseline;
+        SteroidsBaseline = index.ClinicalData?.SteroidsBaseline;
     }
 }
--- Unite.Composer/Visualization/Oncogrid/Data/OncoGridGene.cs
+++ Unite.Composer/Visualization/Oncogrid/Data/OncoGridGene.cs
@@ -6,10 +6,16 @@ public class OncoGridGene
 {
     public string Id { get; set; }
     public string Symbol { get; set; }
+    public string Biotype { get; set; }
+    public string Chromosome { get; set; }
+    public bool? Strand { get; set; }
 
     public OncoGridGene(GeneIndex index)
     {
         Id = index.Id.ToString();
-        Symbol = index.Symbol;
+        Symbol = index.Symbol ?? index.StableId;
+        Biotype = index.Biotype;
+        Chromosome = index.Chromosome;
+        Strand = index.Strand;
     }
 }
