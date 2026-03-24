1ff095b6a873441ec21c4ade278488c57343c702
SemionV <semion.varennik@gmail.com>
2026-03-24 15:03:42 +0100

    Search CNV Profiles

---
+++ Unite.Composer.Web/Resources/Domain/Variants/CnvProfileResource.cs
@@ -0,0 +1,10 @@
+namespace Unite.Composer.Web.Resources.Domain.Variants;
+
+public class CnvProfileResource
+{
+    public string Chromosome { get; set; }
+    public string ChromosomeArm { get; set; }
+    public float Gain { get; set; }
+    public float Loss { get; set; }
+    public float Neutral { get; set; }
+}
