d254e0d4f7b646a2c9d3fbbfe32aacee66b82745
vulasau <valiantsin.ulasau@gmail.com>
2026-05-20 17:24:04 +0200

    Autocomplete basics.

---
+++ Unite.Composer.Web/Controllers/Data/Autocomplete/AutocompleteController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Unite.Composer.Data.Autocomplete;
+using Unite.Data.Context;
+
+namespace Unite.Composer.Web.Controllers.Data.Autocomplete;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class AutocompleteController : Controller
+{
+    private readonly AutocompleteService _autocompleteService;
+
+
+    public AutocompleteController(DomainDbContext dbContext)
+    {
+        _autocompleteService = new AutocompleteService(dbContext);
+    }
+
+    //TODO: Find the way to make this as generic as possible
+    [HttpGet]
+    public async Task<IActionResult> Search([FromQuery] string model, [FromQuery] string field, [FromQuery] string query)
+    {
+        if (string.IsNullOrEmpty(model))
+            return BadRequest("Model is required.");
+
+        if (string.IsNullOrEmpty(field))
+            return BadRequest("Field is required.");
+
+        var comparison = StringComparison.InvariantCultureIgnoreCase;
+
+        string[] results = null;
+
+        if (model.Equals("gene", comparison))
+        {
+            if (field.Equals("symbol", comparison))
+                results = await _autocompleteService.Search<Unite.Data.Entities.Omics.Gene>(gene => gene.Symbol, query);
+        }
+
+        return Ok(results);   
+    } 
+}
